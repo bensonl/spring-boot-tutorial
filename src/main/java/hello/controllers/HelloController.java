992e3ce1550779ec978a1b35a6675fe4a9559cbd
Benson Liu <bliu.developer@gmail.com>
2015-03-08 17:42:04 -0700

    add logging config

--- src/main/java/hello/controllers/HelloController.java
+++ src/main/java/hello/controllers/HelloController.java
@@ -1,6 +1,8 @@
 package hello.controllers;
 
 import hello.services.HelloWorldService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @Autowired
     private HelloWorldService helloWorldService;
 
     @RequestMapping("/")
     public String index() {
-//        return "Greetings from Spring Boot!";
+        log.info("index action begin");
         return helloWorldService.helloWorld();
     }
 
--- src/main/java/hello/services/impl/HelloWorldServiceImpl.java
+++ src/main/java/hello/services/impl/HelloWorldServiceImpl.java
@@ -1,13 +1,18 @@
 package hello.services.impl;
 
 import hello.services.HelloWorldService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by bliu on 3/8/15.
  */
 public class HelloWorldServiceImpl implements HelloWorldService {
 
+    private static final Logger log = LoggerFactory.getLogger(HelloWorldServiceImpl.class);
+
     public String helloWorld() {
+        log.debug("helloWorld - begin");
         return "Hello World!";
     }
 }
